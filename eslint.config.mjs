6189b14a4fbbe2ec1c93bf68e18db8cc7c0fe683
nobumitsu_masugata <ys.training220208@gmail.com>
2024-09-03 23:36:25 +0900

    feat: eslintの設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,42 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import parser from '@typescript-eslint/parser'
 
 export default [
-  { files: ['./src/**/*.{ts}'] },
-  { languageOptions: { globals: globals.es2023 } },
+  { files: ['**/*.ts'] },
+  { ignores: ['dist/**/*', 'vite.config.ts'] },
+  {
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    name: 'custom-rules',
+    files: ['./src/**/*.ts'],
+    rules: [
+      {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: ['function', 'parameter'],
+            format: ['camelCase', 'PascalCase'],
+            leadingUnderscore: 'allow',
+          },
+          {
+            selector: ['variable'],
+            format: ['camelCase', 'UPPER_CASE'],
+          },
+          {
+            selector: 'typeLike',
+            format: ['PascalCase'],
+          },
+        ],
+      },
+    ],
+  },
 ]
