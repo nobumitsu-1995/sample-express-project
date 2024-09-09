48816b88c102fcd2ae331e62dab7fffafba25bcd
nobumitsu_masugata <ys.training220208@gmail.com>
2024-09-09 22:32:42 +0900

    feat: vitestの設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import path from 'path'
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@Controllers": path.resolve(__dirname, "src/Application/Controllers"),
-      "@Usecases": path.resolve(__dirname, "src/Application/Usecases"),
-      "@Entities": path.resolve(__dirname, "src/Domain/Entities"),
-      "@Repositories": path.resolve(__dirname, "src/Domain/Repositories"),
-      "@Databases": path.resolve(__dirname, "src/Infrastructure/Databases"),
-      "@Routers": path.resolve(__dirname, "src/Infrastructure/Routers")
-    }
+      '@Controllers': path.resolve(__dirname, 'src/Application/Controllers'),
+      '@Usecases': path.resolve(__dirname, 'src/Application/Usecases'),
+      '@Entities': path.resolve(__dirname, 'src/Domain/Entities'),
+      '@Repositories': path.resolve(__dirname, 'src/Domain/Repositories'),
+      '@Databases': path.resolve(__dirname, 'src/Infrastructure/Databases'),
+      '@Routers': path.resolve(__dirname, 'src/Infrastructure/Routers'),
+    },
   },
   build: {
     lib: {
       entry: 'src/main.ts',
-      formats: ['cjs']
+      formats: ['cjs'],
     },
     rollupOptions: {
-      external: ['express']
+      external: ['express'],
     },
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+  },
 })
