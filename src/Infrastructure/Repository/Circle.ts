06d4033f749868a04f112e34b2a40d370d2930f3
nobumitsu_masugata <ys.training220208@gmail.com>
2024-10-26 19:00:20 +0900

    feat: Circleのリポジトリを作成

---
+++ src/Infrastructure/Repository/Circle.ts
@@ -0,0 +1,79 @@
+import { PrismaClient } from '@prisma/client'
+import Circle from 'Domain/Models/Circles/Circle'
+import CircleId from 'Domain/Models/Circles/CircleId'
+import CircleName from 'Domain/Models/Circles/CircleName'
+import { ICircleRepository } from 'Domain/Models/Circles/ICircleRepository'
+import UserId from 'Domain/Models/Users/UserId'
+
+export default class CircleRepository implements ICircleRepository {
+  private prisma = new PrismaClient()
+
+  public async findById(id: CircleId): Promise<Circle | null> {
+    const circle = await this.prisma.circle.findUnique({
+      where: { id: id.get() },
+      include: { members: true },
+    })
+
+    if (!circle) {
+      return null
+    }
+
+    return new Circle({
+      id: new CircleId(circle.id),
+      name: new CircleName(circle.name),
+      owner: new UserId(circle.ownerId),
+      members: circle?.members.map((member) => new UserId(member.id)) || [],
+    })
+  }
+
+  public async searchByName(name: CircleName): Promise<Circle[]> {
+    const circles = await this.prisma.circle.findMany({
+      where: {
+        name: {
+          contains: name.get(),
+          mode: 'insensitive',
+        },
+      },
+      include: { members: true },
+    })
+
+    return circles.map(
+      (circle) =>
+        new Circle({
+          id: new CircleId(circle.id),
+          name: new CircleName(circle.name),
+          owner: new UserId(circle.ownerId),
+          members: circle?.members.map((member) => new UserId(member.id)) || [],
+        }),
+    )
+  }
+  public async save(circle: Circle) {
+    await this.prisma.circle.upsert({
+      where: { id: circle.id.get() },
+      update: {
+        name: circle.name.get(),
+        ownerId: circle.owner.get(),
+        members: {
+          connect: circle.getMembers().map((id) => ({
+            id: id.get(),
+          })),
+        },
+      },
+      create: {
+        id: circle.id.get(),
+        name: circle.name.get(),
+        ownerId: circle.owner.get(),
+        members: {
+          connect: circle.getMembers().map((id) => ({
+            id: id.get(),
+          })),
+        },
+      },
+    })
+  }
+  public async delete(id: CircleId) {
+    await this.prisma.circle.delete({
+      where: { id: id.get() },
+    })
+  }
+}
