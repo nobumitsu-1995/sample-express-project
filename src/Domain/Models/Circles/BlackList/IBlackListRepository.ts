d76dbe22754a1c5f3a2079be346ba51242abd754
nobumitsu_masugata <ys.training220208@gmail.com>
2024-10-27 16:11:54 +0900

    feat: BlackListリポジトリの実装

--- src/Domain/Models/Circles/BlackList/IBlackListRepository.ts
+++ src/Domain/Models/Circles/BlackList/IBlackListRepository.ts
@@ -3,8 +3,8 @@ import BlackListId from './BlackListId'
 import CircleId from '../CircleId'
 
 export interface IBlackListRepository {
-  findById: (id: BlackListId) => BlackList
-  findByCircleId: (id: CircleId) => BlackList
+  findById: (id: BlackListId) => Promise<BlackList | null>
+  findByCircleId: (id: CircleId) => Promise<BlackList | null>
   save: (blackList: BlackList) => void
   delete: (id: BlackListId) => void
 }
---
+++ src/Infrastructure/Repository/BlackList.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client'
+import BlackList from 'Domain/Models/Circles/BlackList/BlackList'
+import BlackListId from 'Domain/Models/Circles/BlackList/BlackListId'
+import { IBlackListRepository } from 'Domain/Models/Circles/BlackList/IBlackListRepository'
+import CircleId from 'Domain/Models/Circles/CircleId'
+import UserId from 'Domain/Models/Users/UserId'
+
+export default class BlackListRepository implements IBlackListRepository {
+  private prisma = new PrismaClient()
+
+  public async findById(id: BlackListId): Promise<BlackList | null> {
+    const blackList = await this.prisma.blackList.findUnique({
+      where: { id: id.get() },
+      include: { users: true },
+    })
+
+    if (!blackList) return null
+
+    return new BlackList({
+      id: new BlackListId(blackList.id),
+      circleId: new CircleId(blackList.circleId),
+      users: blackList.users.map((user) => new UserId(user.id)),
+    })
+  }
+
+  public async findByCircleId(id: CircleId): Promise<BlackList | null> {
+    const blackList = await this.prisma.blackList.findUnique({
+      where: { circleId: id.get() },
+      include: { users: true },
+    })
+
+    if (!blackList) return null
+
+    return new BlackList({
+      id: new BlackListId(blackList.id),
+      circleId: new CircleId(blackList.circleId),
+      users: blackList.users.map((user) => new UserId(user.id)),
+    })
+  }
+
+  public async save(blackList: BlackList) {
+    await this.prisma.blackList.upsert({
+      where: { id: blackList.id.get() },
+      update: {
+        circleId: blackList.circleId.get(),
+        users: {
+          connect: blackList.getBlockedUsers().map((id) => ({
+            id: id.get(),
+          })),
+        },
+      },
+      create: {
+        id: blackList.id.get(),
+        circleId: blackList.circleId.get(),
+        users: {
+          connect: blackList.getBlockedUsers().map((id) => ({
+            id: id.get(),
+          })),
+        },
+      },
+    })
+  }
+
+  public async delete(id: BlackListId) {
+    await this.prisma.blackList.delete({
+      where: { id: id.get() },
+    })
+  }
+}
