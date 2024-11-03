5b67122421c047e81512fc589908e6600b99a397
nobumitsu_masugata <ys.training220208@gmail.com>
2024-11-03 19:30:49 +0900

    feat: Userデータを遺sっよに取得するバージョンのRepositoryを追加

--- src/Domain/Models/Circles/ICircleRepository.ts
+++ src/Domain/Models/Circles/ICircleRepository.ts
@@ -1,9 +1,13 @@
+import User from '../Users/User'
 import Circle from './Circle'
 import CircleId from './CircleId'
 import CircleName from './CircleName'
 
 export interface ICircleRepository {
   findById: (id: CircleId) => Promise<Circle | null>
+  findByIdWithUsersData: (
+    id: CircleId,
+  ) => Promise<{ circle: Circle; owner: User; members: User[] } | null>
   findByName: (name: CircleName) => Promise<Circle | null>
   searchByName: (name: CircleName) => Promise<Circle[]>
   save: (circle: Circle) => void
